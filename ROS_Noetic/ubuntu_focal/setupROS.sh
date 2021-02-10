fa00b2c2e8cdabcf4a8f67d67d9c2f6fb3a69773
Leung Machine <45313904+ctm6100@users.noreply.github.com>
2021-02-10 15:11:18 +0000

    Update setupROS.sh
    lol

--- ROS_Noetic/ubuntu_focal/setupROS.sh
+++ ROS_Noetic/ubuntu_focal/setupROS.sh
@@ -1,1 +1,42 @@
-tst
+#!/bin/bash
+test=$(lsb_release -sc)
+echo "${test}"
+if [ "${test}" == "focal" ]; then
+        echo "Ubuntu verison Check...."
+else
+        lsb_release -a
+        echo "this bash is built for Ubuntu 20.0x noetic. Your system is" ${test} 
+        read -p "Do you want to continue?(Y/n)" yn
+        if [ "${yn}" != "Y" ]; then
+                exit
+        fi
+fi
+
+#pre config before intall
+#sources.list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list';
+#Add key 
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654;
+
+#install
+sudo apt update -y;
+sudo apt install -y ros-noetic-desktop-full;
+
+
+source /opt/ros/noetic/setup.bash;
+#add this script every time a new shell is launched
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc;
+source ~/.bashrc;
+
+#install Dependencies for building packages
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential;
+sudo apt install -y python3-rosdep;
+sudo rosdep init;
+rosdep update;
+mkdir -p ~/catkin_ws/src;
+cd ~/catkin_ws/;
+catkin_make;
+rosversion ros;
+source devel/setup.bash;
+echo "Your ROS package:" $ROS_PACKAGE_PATH
+
